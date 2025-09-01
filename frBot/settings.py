e9660e7ff75f0a0a85a23e2bd18d46bcbfd09449
GroverAEP <gespinozaprudencio@gmail.com>
2025-09-01 16:42:12 -0500

    actaulizacion de frontend - A

--- frBot/settings.py
+++ frBot/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -48,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # 👈 aquí va
+
 ]
 
 ROOT_URLCONF = 'frBot.urls'
@@ -119,6 +122,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# Opcional: para compresión y caché
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
